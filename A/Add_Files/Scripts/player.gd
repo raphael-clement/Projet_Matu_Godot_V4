634cec7943fbb59feb99dfdd3662abe30121e92b
raphael-clement <rt.clement@outlook.fr>
2025-04-11 13:45:17 +0200

    Start of inventory
    doesnt work, unable to fix it

--- A/Add_Files/Scripts/player.gd
+++ A/Add_Files/Scripts/player.gd
@@ -5,6 +5,11 @@ const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var interact_ui: CanvasLayer = $InteractUI
+
+
+func _ready() -> void:
+	Global.set_player_reference(self)
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
