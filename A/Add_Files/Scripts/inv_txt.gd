2cc11bf511223c8416983cbc0520221870e2b40c
raphael-clement <rt.clement@outlook.fr>
2025-08-07 14:15:16 +0200

    2h20 de recherche et bidouillage pour un début d'HUD
    HUD -> inventaire + stats + recipes
    je n'arrive pas à afficher du texte dans les onglets -> label + code

---
+++ A/Add_Files/Scripts/inv_txt.gd
@@ -0,0 +1,18 @@
+extends Label
+
+var woodcnt = 0
+var stonecnt = 0
+var ironcnt = 0
+var goldcnt = 0
+var my_label = get_node("Player/Camera2D/HUD/Player Tab/Inventory/Label")
+var my_label.text = "qwertzu"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
